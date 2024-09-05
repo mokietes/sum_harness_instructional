2942d36df5f70fdb512916c493d0f926092a0449
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2024-09-04 20:30:39 -0700

    Update benchmark.cpp

--- benchmark.cpp
+++ benchmark.cpp
@@ -46,7 +46,14 @@ int main(int argc, char** argv)
       t = sum(n, &A[0]);
 
       // insert your end timer code here, and print out elapsed time for this problem size
+      std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
 
+      //elapse time result
+      std::chrono::duration<double> elapsed = end_time - start_time;
+
+      std::cout << " Elapsed time is : " << elapsed.count() << " " << std::endl;
+
+      
       printf(" Sum result = %lld \n",t);
 
    } // end loop over problem sizes
